2b720592dd2735764ecb895dd4d0d91c5e41df35
Thiru Njuguna <thirunjuguna@outlook.com>
2018-11-07 17:21:56 +0300

    rubocop fixup (#6)
    * update rubocop for the template

--- lib/natra/generators/app/templates/db/migrate/add_extensions.rb
+++ lib/natra/generators/app/templates/db/migrate/add_extensions.rb
@@ -1,7 +1,7 @@
 class AddExtensions < ActiveRecord::Migration[5.2]
-    def change
-      enable_extension 'hstore'
-      enable_extension 'uuid-ossp'
-      enable_extension 'pgcrypto'
-    end
+  def change
+    enable_extension 'hstore'
+    enable_extension 'uuid-ossp'
+    enable_extension 'pgcrypto'
+  end
 end
